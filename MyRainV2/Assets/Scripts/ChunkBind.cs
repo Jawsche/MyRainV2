beba429e57f878c4058f6a7f83430c2c9769b916
Jawsche <joshua.allin@live.com>
2018-11-24 13:19:53 +0900

    Movement is Nearly there
    just some overshooting

--- MyRainV2/Assets/Scripts/ChunkBind.cs
+++ MyRainV2/Assets/Scripts/ChunkBind.cs
@@ -9,17 +9,22 @@ public class ChunkBind : MonoBehaviour {
 
     public float moveSpeed = 0.5f;
     private float moveSpeedInit;
-    public float returnStrength = 2000f;
-    public float returnDist = 0f;
+
+
+    public Vector2 idealPosHead = Vector2.up;
+    public float returnStrength = 17f;
+    [Range(0f, 5f)]
+    public float returnDist = 1f;
     [Range (0f,1f)]
     public float returnStrengthRatioA = 0.7f;
     [Range(0f, 1f)]
     public float returnStrengthRatioB;
+    [Range(0f, 1f)]
+    public float lerpTVal = 1f;
 
     public Rigidbody2D rigidBodyA;
     public Rigidbody2D rigidBodyB;
     
-    public Vector2 idealPosA;
 
     private bool p_grounded = false;
     private bool p_standing = false;
@@ -27,11 +32,12 @@ public class ChunkBind : MonoBehaviour {
 
 
    
-    public float a_jumpStrangth = 20.0f;
-    [Range(1, 10)]
-    public float fallMultiplier = 2.5f;
-    [Range(1, 10)]
-    public float lowJumpMultiplier = 2f;
+    //public float a_jumpStrangth = 20.0f;
+    //[Range(1, 10)]
+    //public float fallMultiplier = 2.5f;
+    //[Range(1, 10)]
+    //public float lowJumpMultiplier = 2f;
+    
 
     // Use this for initialization
     void Start () {
@@ -41,19 +47,42 @@ public class ChunkBind : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
-        returnStrengthRatioB = 1f - returnStrengthRatioA;
-        Vector3 target = (new Vector3(otherChunk.transform.position.x + idealPosA.x, otherChunk.transform.position.y + idealPosA.y));
-        float dist = Vector2.Distance(target, transform.position);
-        Vector2 dirVecA = (target - transform.position).normalized;
-        Vector2 dirVecB = -(target - transform.position).normalized;
-        Debug.Log(dist);
+	void FixedUpdate () {
 
-        if (dist > returnDist)
+        if(CrossPlatformInputManager.GetButton("Fire2"))
         {
-            rigidBodyA.velocity += Vector2.Lerp(Vector2.zero,  dirVecA * dist, Time.deltaTime * (returnStrength* returnStrengthRatioA));
-            rigidBodyB.velocity += Vector2.Lerp(Vector2.zero, dirVecB * dist, Time.deltaTime * (returnStrength * returnStrengthRatioB));
+            Vector3 pos = CrossPlatformInputManager.mousePosition;
+            Vector3 mousePt = Camera.main.ScreenToWorldPoint(pos);
+            Vector3 temp = mousePt;
+            temp.z = 0;
+            transform.position = temp;
         }
+
+        returnStrengthRatioB = 1f - returnStrengthRatioA;
+        Vector2 dirVecA;
+        float distA = Vector2.Distance(otherChunk.transform.position, transform.position);
+        if (distA > returnDist)
+        {
+            dirVecA = (otherChunk.transform.position - transform.position).normalized;
+        }
+        else
+        {
+            dirVecA = idealPosHead;
+        }
+        //Debug.Log(dist);
+
+
+
+        //float theta = Time.timeSinceLevelLoad / period;
+        //float distance = amplitude * Mathf.Sin(theta) + 0.5f;
+        ////transform.position = targetA + Vector3.up * distance;
+        //Vector3 sinVal = (Vector3.up ) * distance;
+        //Debug.Log(sinVal);
+
+        
+        rigidBodyA.velocity += Vector2.Lerp(Vector2.zero, dirVecA * distA * returnStrength * returnStrengthRatioA, lerpTVal);
+        rigidBodyB.velocity += Vector2.Lerp(Vector2.zero, -dirVecA * distA * returnStrength * returnStrengthRatioB, lerpTVal);
+        
 
         PlayerInput();
 
@@ -75,43 +104,45 @@ public class ChunkBind : MonoBehaviour {
         if (h != 0)
         {
             rigidBodyA.velocity += new Vector2(moveSpeed * h * returnStrengthRatioA, 0f);
+            rigidBodyB.velocity += new Vector2(moveSpeed * h * returnStrengthRatioB, 0f);
         }
         if (v != 0)
         {
             rigidBodyA.velocity += new Vector2(0f, moveSpeed * v * returnStrengthRatioA);
+            rigidBodyB.velocity += new Vector2(0f, moveSpeed * v * returnStrengthRatioB);
         }
-        //Jump
-        if (CrossPlatformInputManager.GetButtonDown("Jump") && p_grounded)
-        {
-            rigidBodyA.AddForce(Vector2.up * a_jumpStrangth, ForceMode2D.Impulse);
-            rigidBodyB.AddForce(Vector2.up * a_jumpStrangth, ForceMode2D.Impulse);
-        }
-        if (rigidBodyA.velocity.y < 0 && !p_grounded)// maybe not use p_grounded here
-        {
-            rigidBodyA.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
-            rigidBodyB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
-        }
-        else if (rigidBodyA.velocity.y > 0.0f && !CrossPlatformInputManager.GetButton("Jump"))
-        {
-            rigidBodyA.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
-            rigidBodyB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
-        }
+        ////Jump
+        //if (CrossPlatformInputManager.GetButtonDown("Jump") && p_grounded)
+        //{
+        //    rigidBodyA.AddForce(Vector2.up * a_jumpStrangth, ForceMode2D.Impulse);
+        //    rigidBodyB.AddForce(Vector2.up * a_jumpStrangth, ForceMode2D.Impulse);
+        //}
+        //if (rigidBodyA.velocity.y < 0 && !p_grounded)// maybe not use p_grounded here
+        //{
+        //    rigidBodyA.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
+        //    rigidBodyB.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1f) * Time.deltaTime;
+        //}
+        //else if (rigidBodyA.velocity.y > 0.0f && !CrossPlatformInputManager.GetButton("Jump"))
+        //{
+        //    rigidBodyA.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
+        //    rigidBodyB.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
+        //}
 
         //crouching
         if (CrossPlatformInputManager.GetButtonDown("Fire1") && p_standing == true)
         {
             p_standing = false;
             if (p_facingDir == 1.0f)
-                idealPosA = new Vector2(1f, 0f);
+                idealPosHead = new Vector2(1f, 0f);
             else if (p_facingDir == -1.0f)
-                idealPosA = new Vector2(-1f, 0f);
+                idealPosHead = new Vector2(-1f, 0f);
             moveSpeed *= 0.7f;
         }
         //stand back up
         else if (CrossPlatformInputManager.GetButtonDown("Fire1") && p_standing == false)
         {
             p_standing = true;
-            idealPosA = new Vector2(0f, 1f);
+            idealPosHead = new Vector2(0f, 1f);
             moveSpeed = moveSpeedInit;
         }
     }
@@ -134,12 +165,11 @@ public class ChunkBind : MonoBehaviour {
 
     private void OnDrawGizmos()
     {
-        float dist = Vector2.Distance(otherChunk.transform.position, (new Vector3(transform.position.x + idealPosA.x, transform.position.y + idealPosA.y)));
+        //Gizmos.DrawSphere(idealPosA, 0.1f);
+        //Gizmos.DrawSphere(idealPosB, 0.1f);
 
         Debug.DrawLine(transform.position, otherChunk.transform.position, Color.blue);
-        Debug.DrawLine((new Vector3(otherChunk.transform.position.x + idealPosA.x, otherChunk.transform.position.y + idealPosA.y)), otherChunk.transform.position, Color.green);
-        Gizmos.DrawSphere(transform.position, 0.1f);
-        Gizmos.DrawSphere((new Vector3(otherChunk.transform.position.x + idealPosA.x, otherChunk.transform.position.y + idealPosA.y)), 0.1f);
-        Gizmos.DrawSphere(otherChunk.transform.position, 0.1f);
+        Gizmos.DrawSphere(transform.position, 0.05f);
+        Gizmos.DrawSphere(otherChunk.transform.position, 0.05f);
     }
 }
